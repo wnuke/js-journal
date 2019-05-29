89fc2f0ad640e2c5f73422fd8c5861b73ecf35c1
Willem Leitso <willem@leit.so>
2019-05-29 09:46:59 +0200

    Created basic markdown editor.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,77 @@ const {
 var fs = require('fs')
 var path = app.getPath('appData')
 var filepath = path + '/js-journal.json'
+var livedb = require('livedb');
+var sharejs = require('share');
+var backend = livedb.client(livedb.memory());
+var share = require('share').server.createClient({backend: backend});
+var showdown  = require('showdown')
+
+window.onload = function() {
+    var converter = new showdown.Converter();
+    var pad = document.getElementById('pad');
+    var markdownArea = document.getElementById('markdown');
+
+    // make the tab act like a tab
+    pad.addEventListener('keydown',function(e) {
+        if(e.keyCode === 9) { // tab was pressed
+            // get caret position/selection
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            var target = e.target;
+            var value = target.value;
+
+            // set textarea value to: text before caret + tab + text after caret
+            target.value = value.substring(0, start)
+                            + "\t"
+                            + value.substring(end);
+
+            // put caret at right position again (add one for the tab)
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            e.preventDefault();
+        }
+    });
+
+    var previousMarkdownValue;
+
+    // convert text area to markdown html
+    var convertTextAreaToMarkdown = function(){
+        var markdownText = pad.value;
+        previousMarkdownValue = markdownText;
+        html = converter.makeHtml(markdownText);
+        markdownArea.innerHTML = html;
+    };
+
+    var didChangeOccur = function(){
+        if(previousMarkdownValue != pad.value){
+            return true;
+        }
+        return false;
+    };
+
+    // check every second if the text area has changed
+    setInterval(function(){
+        if(didChangeOccur()){
+            convertTextAreaToMarkdown();
+        }
+    }, 1000);
+
+    // convert textarea on input change
+    pad.addEventListener('input', convertTextAreaToMarkdown);
+
+    // ignore if on home page
+    if(document.location.pathname.length < 2){
+        // implement share js
+        sharejs.open(document.location.pathname, 'text', function(error, doc) {
+            doc.attach_textarea(pad);
+            convertTextAreaToMarkdown();
+        });
+    }
+
+    // convert on page load
+    convertTextAreaToMarkdown();
+
+};
