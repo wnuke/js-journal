425b6fad15cbfe0a693746f419f9ec7e780f4d99
willem <willem@leit.so>
2019-12-08 15:55:26 +0100

    Created basic functions for V2

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
+function makeTwoDig(number) {
+  if (number < 10) {
+    return '0' + number
+  } else {
+    return number
+  }
+}
+
+// date ----------->
 var d = new Date();
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -7,14 +16,6 @@ var nowmonth
 var nowyear
 
 Intl.DateTimeFormat().resolvedOptions().timeZone
-
-function makeTwoDig(number) {
-  if (number < 10) {
-    return '0' + number
-  } else {
-    return number
-  }
-}
 
 function markdownDate(year, month, day) {
   var mdd = new Date(year, month - 1, day)
@@ -41,11 +42,65 @@ function getFullDate(year, month, day) {
   var fdate = fday + ' ' + day.toString() + ' ' + fmonth + ' ' + year.toString()
   return fdate
 }
+// end date <---------
 
-function makeTwoDig(number) {
-  if (number < 10) {
-    return '0' + number
-  } else {
-    return number
+const remote = require('electron').remote;
+const app = remote.app;
+
+const fs = require('fs');
+var CryptoJS = require("crypto-js");
+
+var password
+var passwordisset = false
+
+// Make sure JS Journal has access to it's userdata folder ------->
+
+var canrwuserdata
+
+function quitNoAccess() {
+  alert('JS Journal doees no have sufficiant permissions to ' + app.getPath('userData') + ' (its data folder), please make sure JS Journal can read/write here.')
+  app.quit()
+}
+
+fs.promises.access(app.getPath('userData'), fs.constants.R_OK | fs.constants.W_OK)
+  .then(() => canrwuserdata = true)
+  .catch(() => quitNoAccess());
+
+// end permissions check <----------
+
+// Check if user has already registered / created a journal ----->
+
+var hasregistered
+
+if (fs.existsSync(app.getPath('userData')+'/journal/passcheck')) {
+  hasregistered = true
+} else {
+  hasregistered = false
+}
+
+// end registered check <--------
+
+function login() {
+  password = $('#passwd').val()
+  if (passwordisset == false && password != '') {
+    passwordisset = true
   }
+}
+
+$("#passwd").keypress(function (event) {
+  if (event.which == 13) {
+    login()
+  }
+});
+
+function readFile(filepath) {
+  textencrypted = fs.readFileSync(filepath, 'utf-8')
+  textdecrypted = CryptoJS.AES.decrypt(textencrypted, password)
+  textutf8 = textdecrypted.toString(CryptoJS.enc.Utf8)
+  return textutf8
+}
+
+function writeFile(filepath, content) {
+  textencrypted = CryptoJS.AES.encrypt(content, password).toString()
+  fs.writeFileSync(filepath, content)
 }
