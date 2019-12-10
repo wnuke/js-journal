c19c01f8dddd4db070916f435f15e64d32a20a45
willem <willem@leit.so>
2019-12-10 12:43:35 +0100

    added function to create entries

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,8 @@ function makeTwoDig(number) {
 }
 
 // date ----------->
-var d = new Date();
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var nowdate
 var nowday
 var nowmonth
 var nowyear
@@ -18,7 +16,8 @@ var nowyear
 Intl.DateTimeFormat().resolvedOptions().timeZone
 
 function markdownDate(year, month, day) {
-  var mdd = new Date(year, month - 1, day)
+  d = new Date();
+  var mdd = new Date(year, month, day)
   var mddatestr = '<p style="display:none">' + year + '-' + month + '-' + day + '</p>'
   var mdyear = '<h1 style="color:#6EAAD2;line-height: 0px;">' + year + '</span><br>'
   var mddate = '<h2 style="color:#AAAAAA;line-height: 10px;">' + days[mdd.getDay()] + ' ' + day + ' ' + months[mdd.getMonth()] + '</span><br>'
@@ -27,12 +26,18 @@ function markdownDate(year, month, day) {
 }
 
 
-function getTodayDate() {
+function getTodayDateTime() {
+  d = new Date();
   nowday = JSON.stringify(makeTwoDig(d.getDate()))
   nowmonth = makeTwoDig(d.getMonth() + 1)
   nowyear = JSON.stringify(d.getFullYear())
+  nowhours = d.getHours()
+  nowminutes = d.getMinutes()
+  nowseconds = d.getSeconds()
+  nowtime = [nowhours, nowminutes, nowseconds]
   nowdate = [nowyear, nowmonth, nowday]
-  return nowdate
+  nowdatetime = [nowdate, nowtime]
+  return nowdatetime
 }
 
 function getFullDate(year, month, day) {
@@ -69,7 +74,6 @@ function readFile(filepath, pass) {
 function writeFile(filepath, content, pass) {
   textencrypted = CryptoJS.AES.encrypt(content, pass).toString()
   fs.writeFileSync(filepath, textencrypted)
-  fs.writeFileSync(filepath, content)
 }
 
 function strIsInt(str) {
@@ -183,6 +187,7 @@ function journal() {
 // scan for entries ---->
 
 function scanForEntries() {
+  entries = []
   if (!fs.existsSync(entriesfolder)) {
     fs.mkdirSync(entriesfolder)
     scanForEntries()
@@ -204,9 +209,9 @@ var entriesindex
 
 function indexEntries() {
   entriesindex = []
-  for (i=0;i<entries.length;i++) {
+  for (i = 0; i < entries.length; i++) {
     entry = readFile(entries[i], password)
-    if (entry.length>0) {
+    if (entry.length > 0) {
       entrymeta = entry.split('---')[1].split(': ')
       entrytitle = entrymeta[1].split('date')[0].trim()
       entrydate = entrymeta[2].split('time')[0].trim()
@@ -217,3 +222,17 @@ function indexEntries() {
 }
 
 // end index entries <----
+
+function createEntry(title, entrynumber) {
+  datetimearr = getTodayDateTime()
+  date = datetimearr[0][0] + '-' + datetimearr[0][1] + '-' + datetimearr[0][2]
+  time = datetimearr[1][0] + ':' + datetimearr[1][1] + ':' + datetimearr[1][2]
+  entryfilecontent = `---
+title: ${title}
+date: ${date}
+time: ${time}
+---`
+  writeFile(entriesfolder + '/' + entrynumber + '.jsje', entryfilecontent, password)
+  scanForEntries()
+  indexEntries()
+}
